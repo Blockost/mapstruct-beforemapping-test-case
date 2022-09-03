162e54e20b5604a06bd6b67a9d731c60ef9ec306
Blockost <simon.espigole@gmail.com>
2022-09-03 23:52:31 +0200

    fix: adding qualified beforeMapping solves the issue

--- src/main/java/com/blockost/mapstructbeforemappingtestcase/ChildClassMapper.java
+++ src/main/java/com/blockost/mapstructbeforemappingtestcase/ChildClassMapper.java
@@ -3,6 +3,7 @@ package com.blockost.mapstructbeforemappingtestcase;
 import org.mapstruct.BeforeMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Named;
+import org.mapstruct.Qualifier;
 
 import java.util.Set;
 
@@ -17,6 +18,15 @@ public interface ChildClassMapper {
     @BeforeMapping
     default Set<ChildClassDTO> beforeMapping(java.util.Collection<ChildClassEntity> collection) {
         return Set.of();
+    }
+
+
+    // XXX 2022-09-03 Blockost beforeMapping method needs to be qualified the same way as the mapping method
+    // to be picked up. This fixes the issue
+    @Named("qualifier")
+    @BeforeMapping
+    default Set<ChildClassDTO> beforeMappingWithQualifier(java.util.Collection<ChildClassEntity> collection) {
+        return beforeMapping(collection);
     }
 
 }
